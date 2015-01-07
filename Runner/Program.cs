3869f0862e947cc38b6be057794bd4e13d73f9d7
Ben Johnson <ben@hadron.io>
2015-01-07 01:42:33 -0600

    Initial Commit

---
+++ Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var exchangeName = "testExchange";
+                channel.ExchangeDeclare(exchangeName, "fanout");
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queueName, exchangeName, "");
+
+                var consumer = new ObservableBasicConsumer(channel);
+
+                var observable = consumer.Consume<string>(queueName, false, "testConsumer");
+
+                observable.Subscribe(message => Console.WriteLine("Received: {0}", message.Body));
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
