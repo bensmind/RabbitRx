139921815996f91b9ddfd9ff2aeea9ccadcab40e
Darren Pruitt <ddpruitt@users.noreply.github.com>
2018-09-12 12:27:09 -0500

    Refactored Producer into seperate class

--- src/Test/Runners/Runner.Producer/Program.cs
+++ src/Test/Runners/Runner.Producer/Program.cs
@@ -12,55 +12,13 @@ namespace Runner.Producer
 {
     public static class Program
     {
-        private static readonly TestQueueConfiguration TestQueueConfig = TestQueueConfiguration.BuildFanoutExchangeQueue();
-
         
         public static void Main(string[] args)
         {
-            Start();
-            TestQueueConfig.Dispose();
-        }
-
-        private static CancellationTokenSource _tokenSource;
-
-        private static void Start()
-        {
-            _tokenSource = new CancellationTokenSource();
-
-            Console.WriteLine("Rabbit Producer: Press Enter to Start");
-            Console.ReadLine();
-            Task.Run(() => Produce());
-            Console.WriteLine("Press Any Key to Stop");
-            Console.ReadLine();
-            _tokenSource.Cancel();
-            Start();
-        }
-
-        private static void Produce()
-        {
-            var rand = new Random((int) DateTime.UtcNow.Ticks);
-            var settings = new BasicProperties()
+            using (var randomIntProducer = new RandomIntProducer())
             {
-                ContentType = "text/plain",
-                DeliveryMode = 1 //1)not durable, 2)durable
-            };
-
-            var ob = Observable.Generate(rand.Next(),
-                i => !_tokenSource.IsCancellationRequested,
-                i => rand.Next(),
-                i => i,
-                x => TimeSpan.FromMilliseconds(rand.Next(250)));
-
-            ob.Subscribe(num =>
-            {
-                var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(num));
-                TestQueueConfig?.Channel.BasicPublish(
-                    exchange: TestQueueConfig.ExchangeName, 
-                    routingKey: "",
-                    basicProperties: settings, 
-                    body: bytes);
-                Console.WriteLine("Published: {0}", num);
-            }, _tokenSource.Token);
+                randomIntProducer.Start();
+            }
         }
     }
 }
---
+++ src/Test/Runners/Runner.Producer/RandomIntProducer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+using Runner.Common;
+
+namespace Runner.Producer
+{
+    public class RandomIntProducer : IDisposable
+    {
+        private static readonly TestQueueConfiguration TestQueueConfig = TestQueueConfiguration.BuildFanoutExchangeQueue();
+
+        private static CancellationTokenSource _tokenSource;
+
+        public void Start()
+        {
+            _tokenSource = new CancellationTokenSource();
+
+            Console.WriteLine("Rabbit Producer: Press Enter to Start");
+            Console.ReadLine();
+            Task.Run(() => Produce());
+            Console.WriteLine("Press Any Key to Stop");
+            Console.ReadLine();
+            _tokenSource.Cancel();
+            Start();
+        }
+
+        public void Produce()
+        {
+            var rand = new Random((int)DateTime.UtcNow.Ticks);
+            var settings = new BasicProperties()
+            {
+                ContentType = "text/plain",
+                DeliveryMode = 1 //1)not durable, 2)durable
+            };
+
+            var ob = Observable.Generate(rand.Next(),
+                i => !_tokenSource.IsCancellationRequested,
+                i => rand.Next(),
+                i => i,
+                x => TimeSpan.FromMilliseconds(rand.Next(250)));
+
+            ob.Subscribe(num =>
+            {
+                var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(num));
+                TestQueueConfig?.Channel.BasicPublish(
+                    exchange: TestQueueConfig.ExchangeName,
+                    routingKey: "",
+                    basicProperties: settings,
+                    body: bytes);
+                Console.WriteLine("Published: {0}", num);
+            }, _tokenSource.Token);
+        }
+
+        public void Dispose()
+        {
+            TestQueueConfig?.Dispose();
+        }
+    }
+}
